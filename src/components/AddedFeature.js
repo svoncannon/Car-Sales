4cd77dd8b2a8e7c8456ff19450ecad457eb905cb
Samantha <toxicwow21@gmail.com>
2020-11-04 16:05:21 -0500

    finished MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { removeFeature } from '../actions/actions';
+
 
 const AddedFeature = props => {
+  const { feature, removeFeature } = props;
+
+
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
+      <button onClick={() => removeFeature(feature)}className="button">X</button>
+      {feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, { removeFeature })(AddedFeature);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// redux related imports
+import { createStore } from 'redux';
+import { reducer } from './reducers/carReducer';
+
+// joiner imports
+import { Provider } from 'react-redux';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+export const store = createStore(reducer);
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
